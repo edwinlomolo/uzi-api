f488b1e6c38e76628869589c09123730e60ce148
3dw1nM0535 <workockmoses@gmail.com>
2024-01-23 21:37:12 +0300

    fix: nil for error

--- services/route/route_client.go
+++ services/route/route_client.go
@@ -21,30 +21,35 @@ func NewRouteService(logger *logrus.Logger, store *sqlStore.Queries) {
 func GetRouteService() Route { return routeService }
 
 func (r *routeClient) GetTripRoute(input model.TripRouteInput) (*model.TripRoute, error) {
-	_, pickupErr := r.parsePickupDropoff(*input.Pickup)
+	pickup, pickupErr := r.parsePickupDropoff(*input.Pickup)
 	if pickupErr != nil {
 		return nil, pickupErr
 	}
 
-	_, dropoffErr := r.parsePickupDropoff(*input.Dropoff)
+	dropoff, dropoffErr := r.parsePickupDropoff(*input.Dropoff)
 	if dropoffErr != nil {
 		return nil, dropoffErr
 	}
 
-	return nil, nil
+	return r.computeRoute(*pickup, *dropoff)
 }
 
-func (r *routeClient) parsePickupDropoff(input model.TripInput) (model.Geocode, error) {
+func (r *routeClient) parsePickupDropoff(input model.TripInput) (*model.Geocode, error) {
 	geo := &model.Geocode{}
 
+	// Google place autocomplete select won't have cord in the request
 	if input.Location.Lat == 0.0 && input.Location.Lng == 0.0 {
 		placedetails, err := location.GetLocationService().GetPlaceDetails(input.PlaceID)
 		if err != nil {
-			return model.Geocode{}, err
+			return nil, err
 		}
 
 		geo = placedetails
 	}
 
-	return *geo, nil
+	return geo, nil
+}
+
+func (r *routeClient) computeRoute(pickup, dropoff model.Geocode) (*model.TripRoute, error) {
+	return nil, nil
 }
