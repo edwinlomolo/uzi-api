9f5557a8fe76b71ec77c9de9f5f97402df94ecba
edwinlomolo <workockmoses@gmail.com>
2024-03-06 19:13:39 +0300

    fix: report sentry logging in staging/production environment only

--- middleware/sentry.go
+++ middleware/sentry.go
@@ -3,12 +3,25 @@ package middleware
 import (
 	"net/http"
 
+	"github.com/edwinlomolo/uzi-api/config"
 	sentryHttp "github.com/getsentry/sentry-go/http"
 )
 
 func Sentry(next http.Handler) http.Handler {
 	return http.HandlerFunc(
 		func(w http.ResponseWriter, r *http.Request) {
-			sentryHttp.New(sentryHttp.Options{}).Handle(next).ServeHTTP(w, r)
+			if isStaging() || isProd() {
+				sentryHttp.New(sentryHttp.Options{}).Handle(next).ServeHTTP(w, r)
+			} else {
+				next.ServeHTTP(w, r)
+			}
 		})
+}
+
+func isStaging() bool {
+	return config.Config.Server.Env == "staging"
+}
+
+func isProd() bool {
+	return config.Config.Server.Env == "production"
 }
