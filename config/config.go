eb28a7bad9f3fad8cc3b25e418a056ec198745d7
edwinlomolo <workockmoses@gmail.com>
2024-03-11 15:39:10 +0300

    chore: cleanup

--- config/config.go
+++ config/config.go
@@ -151,7 +151,6 @@ func googleConfig() Google {
 	config.GooglePlacesApiKey = strings.TrimSpace(os.Getenv("MAPS_PLACES_API_KEY"))
 	config.GoogleGeocodeApiKey = strings.TrimSpace(os.Getenv("MAPS_GEOCODE_API_KEY"))
 	config.GoogleRoutesApiKey = strings.TrimSpace(os.Getenv("MAPS_ROUTES_API_KEY"))
-	config.GoogleCloudStorageApiKey = strings.TrimSpace(os.Getenv("GOOGLE_CLOUD_STORAGE_API_KEY"))
 	config.GoogleCloudStorageCourierDocumentsBucket = strings.TrimSpace(os.Getenv("GOOGLE_CLOUD_STORAGE_COURIER_DOCUMENTS_BUCKET"))
 	config.GoogleApplicationDevelopmentCredentials = strings.TrimSpace(os.Getenv("GOOGLE_CLOUD_ADC"))
 	config.GoogleCloudObjectUri = strings.TrimSpace(os.Getenv("GOOGLE_CLOUD_OBJECT_URI"))
